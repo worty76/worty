14d9b3426429fe5ec6efd28977c781a976265509
Worty <lethanhdat762003@gmail.com>
2025-02-10 13:49:34 +0700

    feat: update about

--- next.config.js
+++ next.config.js
@@ -11,13 +11,13 @@ const nextConfig = {
     ],
   },
   env: {
-    apiKey: process.env.NEXT_PUBLIC_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_authDomain,
-    projectId: process.env.NEXT_PUBLIC_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_appId,
-    measurementId: process.env.NEXT_PUBLIC_measurementId,
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
   },
 };
 
